6bc670b807085ac7db4aa3281e1eecfbc441a4a2
Ryan <ryanoviski@gmail.com>
2026-06-18 12:11:04 -0300

    feat: add application window icon

--- src/main/java/com/hyperion/app/MainApp.java
+++ src/main/java/com/hyperion/app/MainApp.java
@@ -4,9 +4,14 @@ import com.hyperion.config.DatabaseInitializer;
 import com.hyperion.service.AppSettingsService;
 import com.hyperion.util.SceneManager;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class MainApp extends Application {
+
+    private static final String APP_ICON = "/images/app-icon.png";
 
     private final AppSettingsService appSettingsService = new AppSettingsService();
 
@@ -15,6 +20,9 @@ public class MainApp extends Application {
         DatabaseInitializer.initialize();
 
         stage.setTitle("Hyperion");
+        stage.getIcons().add(new Image(Objects.requireNonNull(
+                MainApp.class.getResourceAsStream(APP_ICON)
+        )));
         SceneManager.setStage(stage);
         SceneManager.switchTo(getInitialView());
         stage.show();
